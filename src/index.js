31c50c3afdb581e16eb2bf2dd54945e05c2044e6
Kevin Chaney <kevin_chaney@icloud.com>
2022-06-07 12:06:03 -0700

    commit tree 0d936bf2cc9664cb99eca6ba942a498e03960be1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,81 @@
-console.log('%c HI', 'color: firebrick')
+// Challenge 1
+const container = document.querySelector("#dog-image-container")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+let ulContainer = document.querySelector("#dog-breeds")
+const dropdown = document.querySelector("#breed-dropdown")
+let breedsArray = []
+
+dropdown.addEventListener('change', handleChange)
+
+//fetch API for images
+function getImages(){
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(images => {
+        const imgs = images.message
+        let imgsArray = createImgElement(imgs)
+        renderElement(imgsArray)
+    })
+}
+
+function createImgElement(imgs){
+    return imgs.map((img) => {
+        let i = `<img src=${img}>`
+        return i
+    })
+}
+
+function renderImgs(imgsArray){
+   imgsArray.forEach(element => {
+       renderElement(element)
+   })
+}
+
+function renderElement(element){
+    ulContainer.innerHTML += element
+}
+
+function getBreeds(){
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(breeds => {
+        breedsArray = Object.keys(breeds.message)
+        const breedsLis = createLiElement(breedsArray)
+        renderLis(breedsLis)
+    })
+}
+
+function createLiElement(breedsArray){
+    return breedsArray.map((breed) => {
+        let li = `<li>${breed}</li>`
+        return li
+    })
+}
+
+function renderLis(breedsLis){
+    breedsLis.forEach(element => {
+        renderElement(element)
+    })
+ }
+
+function handleChange(event){
+    const letter = event.target.value 
+    const filteredBreeds = breedsArray.filter(breed => breed.startsWith(letter))
+    const filteredBreedsLis = createLiElement(filteredBreeds);
+    ulContainer.innerHTML = ''
+    renderLis(filteredBreedsLis)
+
+}
+
+//getImages();
+
+getBreeds();
+ 
+    const colorArray = ['red', 'orange', 'green', 'blue', 'purple']
+    ulContainer.addEventListener('click', (event) => {
+        const randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
+        event.target.style.color = randomColor
+    })
+
+
